a75b68d9a45ed85a9a49e58227bef8a7ccc49897
alexanderbkl <asederado@gmail.com>
2023-10-13 04:46:08 +0200

    added s3 key head check if files exist in information pool, return files metadata that were already existing, facilitate folder memoization

--- internal/form/file_upload.go
+++ internal/form/file_upload.go
@@ -1,1 +1,41 @@
 package form
+
+import "github.com/Hello-Storage/hello-back/internal/entity"
+
+type BaseMeta struct {
+	ID               int              `json:"id"`
+	CreatedAt        string           `json:"created_at"`
+	DeletedAt        *string          `json:"deleted_at,omitempty"`
+	UpdatedAt        string           `json:"updated_at"`
+	EncryptionStatus entity.EncryptionStatus `json:"encryption_status"`
+	Decrypted        *bool            `json:"decrypted,omitempty"`
+}
+
+type CustomFileMeta struct {
+	BaseMeta
+	UID                     string  `json:"uid"`
+	CID                     string  `json:"cid"`
+	CIDOriginalEncrypted    string  `json:"cid_original_encrypted"`
+	CIDOriginalEncryptedB64 *string `json:"cid_original_encrypted_base64_url,omitempty"`
+	Name                    string  `json:"name"`
+	Root                    string  `json:"root"`
+	MimeType                string  `json:"mime_type"`
+	Size                    int64   `json:"size"`
+	MediaType               string  `json:"media_type"`
+	Path                    string  `json:"path"`
+	Data                    *string `json:"data,omitempty"`
+}
+
+type FileResponse struct {
+	ID                   uint                    `json:"id"`
+	Name                 string                  `json:"name"`
+	UID                  string                  `json:"uid"`
+	Root                 string                  `json:"root"`
+	CID                  string                  `json:"cid"`
+	CIDOriginalEncrypted *string                 `json:"cid_original_encrypted"`
+	Mime                 string                  `json:"mime"`
+	Size                 int64                   `json:"size"`
+	EnryptionStatus      entity.EncryptionStatus `json:"encryption_status"`
+	CreatedAt			string                  `json:"created_at"`
+	UpdatedAt			string                  `json:"updated_at"`
+}
