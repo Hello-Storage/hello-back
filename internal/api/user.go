601844d52c5ee9df73174ce5c22c9b4e412d86d0
alexanderbkl <asederado@gmail.com>
2023-11-11 01:27:51 +0100

    add user and ns count endpoint

--- internal/api/user.go
+++ internal/api/user.go
@@ -31,3 +31,41 @@ func UpdateUser(router *gin.RouterGroup) {
 		)
 	})
 }
+
+// GetUserCount returns the total number of registered users and referred users.
+
+// GET /api/user/count
+func GetUserCount(router *gin.RouterGroup) {
+	router.GET("/user/count", func(ctx *gin.Context) {
+	//use gin to count user
+	var count int64
+
+	var user entity.User
+
+	count, err := user.Count()
+
+	if err != nil {
+		AbortInternalServerError(ctx)
+		return
+	}
+
+	var referredCount int64
+
+	var referredUser entity.ReferredUser
+
+	referredCount, err = referredUser.Count()
+
+	if err != nil {
+		AbortInternalServerError(ctx)
+		return
+	}
+
+	ctx.JSON(
+		http.StatusOK,
+		gin.H{
+			"user_count":      count,
+			"ns":  referredCount,
+		},
+	)
+})
+}
