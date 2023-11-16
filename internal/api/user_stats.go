811c805825b7c4675b8694e63eb19956513050c2
alexanderbkl <asederado@gmail.com>
2023-11-16 21:01:39 +0100

    15 seconds instead of 1 second

--- internal/api/user_stats.go
+++ internal/api/user_stats.go
@@ -91,7 +91,7 @@ func (s *SharedUsersData) startUsersBackgroundJob() {
 				usersMutex.Lock()
 				s.WeeklyStatistics = newStats
 				usersMutex.Unlock()
-				time.Sleep(1 * time.Second) // Example delay
+				time.Sleep(15 * time.Second) // Example delay
 			}
 		}
 	}()
