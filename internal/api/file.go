81eb4bb8c07b39efd24cabe70919793b3868d6b1
alexanderbkl <asederado@gmail.com>
2024-02-14 05:46:46 +0100

    changed share file email username sender

--- internal/api/file.go
+++ internal/api/file.go
@@ -500,7 +500,7 @@ func PublishFile(router *gin.RouterGroup) {
 		if shareType == "email" {
 
 			// Send email with the file link to the user and pass also the sender user's email
-			sendEmailLinkToUser(authPayload.UserName, accountIdentifier, newFile, publicFile)
+			sendEmailLinkToUser(authPayload.UserName, shareWithUser, accountIdentifier, newFile, publicFile)
 		}
 
 		// Save the updated shareState.PublicFile
@@ -534,7 +534,7 @@ func formatBytes(size int64) string {
 	return fmt.Sprintf("%.1f %cB", float64(size)/float64(div), "KMGTPE"[exp])
 }
 
-func sendEmailLinkToUser(username string, email string, file *entity.File, publicFile *entity.PublicFileUserShared) {
+func sendEmailLinkToUser(username string, user *entity.User, email string, file *entity.File, publicFile *entity.PublicFileUserShared) {
 
 	mg := mg.Mailgun{
 		Domain: "hello.app",
@@ -544,14 +544,14 @@ func sendEmailLinkToUser(username string, email string, file *entity.File, publi
 	mg.Init()
 	id, err := mg.SendEmail(
 		"noreply@hello.app",
-		email, // Fix: Pass the string value of user.Email
+		email,
 		"hello.app | Received file named "+file.Name+"",
 		"received-file",
 		map[string]interface{}{
 			"filename": file.Name,
 			"filesize": formatBytes(file.Size),
 			"filelink": "https://hello.app/space/shared/public/" + publicFile.ShareHash,
-			"username": email,
+			"username": user.Email.Email,
 		},
 	)
 
