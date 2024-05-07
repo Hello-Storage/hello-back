327c9c63925a69c455db397b389ce76b424f2a9f
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2024-05-07 16:03:00 -0500

    Create invest tables

--- internal/entity/invest_code.go
+++ internal/entity/invest_code.go
@@ -5,9 +5,9 @@ import (
 )
 
 type InvestCode struct {
-	Code           string          `gorm:"primarykey"                      json:"code"`
-	SocialNetwork  string          `gorm:"type:varchar(64)"                json:"social_network"`
-	InvestAccounts []InvestAccount `gorm:"foreignKey:Code;references:Code"`
+	Code          string `gorm:"primarykey"                      json:"code"`
+	SocialNetwork string `gorm:"type:varchar(64)"                json:"social_network"`
+	// InvestAccounts []InvestAccount `gorm:"foreignKey:Code;references:Code"`
 }
 
 // TableName returns the entity table name.
