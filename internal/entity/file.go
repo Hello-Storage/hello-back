a9813b67ca5f135d6ce95ac704fbe032621995f1
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-05-09 10:01:10 -0500

    fix: gitignore fix, air config unnecesary privacy, filesharestate migration error fixed, column ipfs_hash created on files table

--- internal/entity/file.go
+++ internal/entity/file.go
@@ -39,6 +39,7 @@ type File struct {
 	DeletedAt            gorm.DeletedAt `gorm:"index"                               json:"deleted_at"`
 	Path                 string         `gorm:"type:varchar(1024);"                 json:"path"` // full path
 	IsInPool             *bool          `gorm:"type:boolean;default:false;"         json:"is_in_pool"`
+	IPFSHash             *string        `gorm:"type:varchar(256);default:NULL"                    json:"ipfs_hash"`
 	//sharestates are referenced by this file's UID at file share state
 	FileShareState FileShareState `gorm:"foreignKey:FileUID;references:UID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;" json:"file_share_state"`
 	EncryptionStatus EncryptionStatus `gorm:"type:encryption_status;default:'public'" json:"encryption_status"`
